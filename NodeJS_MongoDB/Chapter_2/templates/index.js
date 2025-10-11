e239fe29a10f57f50ac02121e807d87396f25dba
istiakAHMEDsaad <istik2020@gmail.com>
2025-10-11 16:53:01 +0600

    add prettier config

--- NodeJS_MongoDB/Chapter_2/templates/index.js
+++ NodeJS_MongoDB/Chapter_2/templates/index.js
@@ -1,39 +1,39 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-const slugify = require('slugify')
+const slugify = require('slugify');
 
-const replaceTemplate = require('./module/replaceTemplate')
+const replaceTemplate = require('./module/replaceTemplate');
 
 // top level code execute one time (synchronus)
-const tempOverview = fs.readFileSync(`../templates/template-overview.html`,'utf-8');
+const tempOverview = fs.readFileSync(`../templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`../templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`../templates/template-product.html`,'utf-8');
+const tempProduct = fs.readFileSync(`../templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`../dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const slug = dataObj.map(el => slugify(el.productName, {lower: true}))
-console.log(slug)
+const slug = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slug);
 
 // SERVER
 const server = http.createServer((req, res) => {
-  const {query, pathname} = url.parse(req.url, true)
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview Page
   if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'content-type': 'text/html' });
 
     const cardHtml = dataObj.map((el) => replaceTemplate(tempCard, el)); // replace template
-    const output = tempOverview.replace('{%PRODUCT_CARD%}', cardHtml)
+    const output = tempOverview.replace('{%PRODUCT_CARD%}', cardHtml);
 
     res.end(output);
 
     // Product Page
   } else if (pathname === '/product') {
-    res.writeHead(200, {"content-type": "text/html"});
+    res.writeHead(200, { 'content-type': 'text/html' });
     const product = dataObj[query.id];
-    const output = replaceTemplate(tempProduct, product)
+    const output = replaceTemplate(tempProduct, product);
 
     res.end(output);
 
