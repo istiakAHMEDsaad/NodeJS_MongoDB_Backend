c2611e25b7ee7456d048c5a1e21194242448aae1
istiakAHMEDsaad <istik2020@gmail.com>
2025-10-15 01:15:12 +0600

    nodejs cache methods

--- NodeJS_MongoDB/Chapter_4/modules.js
+++ NodeJS_MongoDB/Chapter_4/modules.js
@@ -1,12 +1,18 @@
 // console.log(arguments);
 // console.log(require('module').wrapper);
 
-const Calculator = require('./test-module');
+const Calculator = require('./test-module-1');
 
 // module.export
 const calc1 = new Calculator();
 console.log(calc1.add(10, 20));
 
 // exports
-const calc2 = require('./test-module-2');
-console.log(calc2.add(2, 5));
+// const calc2 = require('./test-module-2');
+const { add, substract, multiply, divide } = require('./test-module-2');
+console.log(add(2, 5));
+
+// caching
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();
