3b18f47eb64d191b535a230cf52a77d3e2c069f5
istiakAHMEDsaad <istik2020@gmail.com>
2025-10-15 01:04:37 +0600

    export methods

--- NodeJS_MongoDB/Chapter_4/modules.js
+++ NodeJS_MongoDB/Chapter_4/modules.js
@@ -3,5 +3,10 @@
 
 const Calculator = require('./test-module');
 
+// module.export
 const calc1 = new Calculator();
 console.log(calc1.add(10, 20));
+
+// exports
+const calc2 = require('./test-module-2');
+console.log(calc2.add(2, 5));
