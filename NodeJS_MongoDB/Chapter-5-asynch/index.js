ed7f4aa4ab0be6aeb9b28ea67ee4d7162c93fbb0
istiakAHMEDsaad <istik2020@gmail.com>
2025-10-15 14:15:34 +0600

    flat structure of chain promises

--- NodeJS_MongoDB/Chapter-5-asynch/index.js
+++ NodeJS_MongoDB/Chapter-5-asynch/index.js
@@ -1,18 +1,38 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log(`Random dog image saved to file!`);
-      });
+const readFilePromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('File not found!');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) return reject('Could not write file');
+      resolve('success');
+    });
+  });
+};
+
+// return promise
+readFilePromise(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePromise('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
