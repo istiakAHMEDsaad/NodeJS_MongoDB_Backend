6653ac67a5f9e3e4cefc4deb1253d90dc4f34351
istiakAHMEDsaad <istik2020@gmail.com>
2025-10-17 23:45:32 +0600

    post method

--- NodeJS_MongoDB/Chapter-6-natours_API/app.js
+++ NodeJS_MongoDB/Chapter-6-natours_API/app.js
@@ -1,22 +1,59 @@
 const fs = require('fs');
 const express = require('express');
+
 const app = express();
-const port = 3000;
+
+//middleware
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// TODO1: get all tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
+    // status 200 means ok
     status: 'success',
     results: tours.length,
     data: {
-      tours //tours: tours 'if its same value we dont need to define'
-    }
-  })
+      tours, //tours: tours 'if its same value we dont need to define'
+    },
+  });
 });
 
+// TODO2: get single tour
+app.get(`/api/v1/tours/:id`, (req, res) => {
+  console.log(req.params);
+  res.status(200).json({
+    status: 'successs',
+  });
+});
+
+// TODO3: post methods
+app.post('/api/v1/tours', (req, res) => {
+  // for local database add id manually
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      // status 201 means created
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
